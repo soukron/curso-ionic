81ded10fcc6000df9664c4f3c8175ce94c061553
Sergio G <soukron@gmbros.net>
2019-11-10 23:29:52 +0100

    Fin de modulo 53

--- src/app/pages/inicio/inicio.page.ts
+++ src/app/pages/inicio/inicio.page.ts
@@ -48,6 +48,11 @@ export class InicioPage implements OnInit {
       icon: 'car',
       name: 'Fabs',
       redirectTo: '/fab'
+    },
+    {
+      icon: 'grid',
+      name: 'Grid - Rows',
+      redirectTo: '/grid'
     }
   ];
 
