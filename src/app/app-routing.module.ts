4bfec6d059169aca473308cd749b731084c03b9f
Sergio G <soukron@gmbros.net>
2019-11-12 17:28:49 +0100

    Fin de modulo 57

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,7 +14,8 @@ const routes: Routes = [
   { path: 'fab', loadChildren: './pages/fab/fab.module#FabPageModule' },
   { path: 'grid', loadChildren: './pages/grid/grid.module#GridPageModule' },
   { path: 'infinite-scroll', loadChildren: './pages/infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule' },
-  { path: 'input', loadChildren: './pages/input/input.module#InputPageModule' }
+  { path: 'input', loadChildren: './pages/input/input.module#InputPageModule' },
+  { path: 'list', loadChildren: './pages/list/list.module#ListPageModule' }
 ];
 
 @NgModule({
