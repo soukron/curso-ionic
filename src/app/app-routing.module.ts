b1d7a1561de621832c68a5c6a726480a872c1786
Sergio G <soukron@gmbros.net>
2019-11-12 23:45:33 +0100

    Fin de modulo 60

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,8 @@ const routes: Routes = [
   { path: 'grid', loadChildren: './pages/grid/grid.module#GridPageModule' },
   { path: 'infinite-scroll', loadChildren: './pages/infinite-scroll/infinite-scroll.module#InfiniteScrollPageModule' },
   { path: 'input', loadChildren: './pages/input/input.module#InputPageModule' },
-  { path: 'list', loadChildren: './pages/list/list.module#ListPageModule' }
+  { path: 'list', loadChildren: './pages/list/list.module#ListPageModule' },
+  { path: 'list-reorder', loadChildren: './pages/list-reorder/list-reorder.module#ListReorderPageModule' }
 ];
 
 @NgModule({
