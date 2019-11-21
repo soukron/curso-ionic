d6819689508b6c13c3765d658ab91f2e5bebc6df
Sergio G <soukron@gmbros.net>
2019-11-21 21:15:34 +0100

    Fin de modulo 96 - NoticiasApp

--- 04-noticias/src/app/services/noticias.service.ts
+++ 04-noticias/src/app/services/noticias.service.ts
@@ -9,6 +9,6 @@ export class NoticiasService {
   constructor( private http: HttpClient ) { }
 
   getTopHeadlines() {
-    return this.http.get(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d2517b840a1a47a8a6ed79cae6fcaad9`);
+    return this.http.get<RespuestaTopHeadlines>(`https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=d2517b840a1a47a8a6ed79cae6fcaad9`);
   }
 }
