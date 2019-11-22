3bb5add4af744e8f36f82c3a8ee7ccea147ae792
Sergio G <soukron@gmbros.net>
2019-11-22 18:15:11 +0100

    Fin de modulo 105 - NoticiasApp

--- 04-noticias/src/app/components/noticia/noticia.component.ts
+++ 04-noticias/src/app/components/noticia/noticia.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Article } from 'src/app/interfaces/interfaces';
 
+import { ActionSheetController } from '@ionic/angular';
+
 import { InAppBrowser } from '@ionic-native/in-app-browser/ngx';
 
 @Component({
@@ -13,11 +15,42 @@ export class NoticiaComponent implements OnInit {
   @Input() noticia: Article;
   @Input() indice: number;
 
-  constructor( private iab: InAppBrowser ) { }
+  constructor(
+      private iab: InAppBrowser,
+      private actionSheetCtrl: ActionSheetController) { }
 
   ngOnInit() {}
 
   abrirNoticia() {
     const browser = this.iab.create(this.noticia.url, '_system');
+  }
+
+  async lanzarMenu() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      buttons: [{
+        text: 'Share',
+        icon: 'share',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        cssClass: 'action-dark',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        cssClass: 'action-dark',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 }
