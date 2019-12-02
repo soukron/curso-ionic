caa86e51ddb671174f27431bf936639a89036af2
Sergio G <soukron@gmbros.net>
2019-12-02 12:47:04 +0100

    Fin de modulo 128 - PeliculasApp

---
+++ 05-peliculasApp/src/app/services/movies.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MoviesService {
+
+  constructor( private http: HttpClient ) { }
+
+  getFeature() {
+    // tslint:disable-next-line: max-line-length
+    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&language=es&api_key=cb354ad4f7919ab27e6b4eb61f196c23`);
+    
+  }
+}
