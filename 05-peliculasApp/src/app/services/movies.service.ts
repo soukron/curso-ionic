0246ed934fa5ff12112aafa1b9731239ff7e9854
Sergio G <soukron@gmbros.net>
2019-12-02 12:54:22 +0100

    Fin de modulo 129 - PeliculasApp

--- 05-peliculasApp/src/app/services/movies.service.ts
+++ 05-peliculasApp/src/app/services/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { RespuestaMDB } from '../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,6 @@ export class MoviesService {
 
   getFeature() {
     // tslint:disable-next-line: max-line-length
-    return this.http.get(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&language=es&api_key=cb354ad4f7919ab27e6b4eb61f196c23`);
-    
+    return this.http.get<RespuestaMDB>(`https://api.themoviedb.org/3/discover/movie?primary_release_date.gte=2019-01-01&primary_release_date.lte=2019-01-31&language=es&api_key=cb354ad4f7919ab27e6b4eb61f196c23`);
   }
 }
--- 05-peliculasApp/src/app/tab1/tab1.page.ts
+++ 05-peliculasApp/src/app/tab1/tab1.page.ts
@@ -1,5 +1,6 @@
 import { OnInit, Component } from '@angular/core';
 import { MoviesService } from '../services/movies.service';
+import { Pelicula } from '../interfaces/interfaces';
 
 @Component({
   selector: 'app-tab1',
@@ -8,11 +9,15 @@ import { MoviesService } from '../services/movies.service';
 })
 export class Tab1Page implements OnInit {
 
-  constructor( private moviesService: MoviesService ) {}
+  peliculasRecientes: Pelicula[] = [];
 
+  constructor( private moviesService: MoviesService ) {}
 
   ngOnInit() {
     this.moviesService.getFeature()
-      .subscribe( console.log );
+      .subscribe( resp => {
+        console.log('respuesta', resp);
+        this.peliculasRecientes = resp.results;
+      } );
   }
 }
